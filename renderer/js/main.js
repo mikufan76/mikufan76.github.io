1badf8629c798a00d68d46b415c84ab5d42b48ad
Orion DiLorenzo <56976587+mikufan76@users.noreply.github.com>
2025-02-06 23:44:59 -0500

    addin ortho camera coords

--- renderer/js/main.js
+++ renderer/js/main.js
@@ -16,9 +16,9 @@ const bg = [62, 144, 235];
 
   // Create and add a container to the stage
   const container = new Container();
-  const camera = new Camera([-1, 2, 1], [0, 0, 0], [1, 1, 1], [0, 0, 0], [0, 1, 0], width, height, app.stage, bg);
+  const camera = new Camera([-8.829, 3.680, 9.392], [-5.73, -3.14, -0.31], [1, 1, 1], [0, 0, 0], [0, 1, 0], width, height, app.stage, bg);
   camera.drawBackground()
-  const sphereOne = new Sphere([0, 0, 0], [0, 0, 0], [2, 2, 2])
+  const sphereOne = new Sphere([-8.902, 3.851, 3.401], [0, 0, 0], [2, 2, 2])
   camera.objects.push(sphereOne)
   camera.transformAllObjects()
 
